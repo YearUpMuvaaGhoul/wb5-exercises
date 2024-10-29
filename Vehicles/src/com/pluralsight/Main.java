266b847b2394fcfcdf4e390997fb4725b6d4f43d
BloopRockets <151418589+BloopRockets@users.noreply.github.com>
2024-10-29 11:21:35 -0400

    did vehicles code

--- Vehicles/src/com/pluralsight/Main.java
+++ Vehicles/src/com/pluralsight/Main.java
@@ -1,17 +1,23 @@
 package com.pluralsight;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        System.out.println("Hello world!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+
+        Hovercraft vehicle1 = new Hovercraft();
+        vehicle1.setElectric(true);
+        vehicle1.setNumberOfPassengers(6);
+        vehicle1.setHoverHeight(2f);
+
+        SemiTruck vehicle2 = new SemiTruck();
+        vehicle2.setElectric(false);
+        vehicle2.setGasoline(true);
+        vehicle2.setHasTrailer(true);
+
+
+
+        System.out.println(vehicle1);
+        System.out.println(vehicle2);
     }
 }
