2f2d04d7910cdd2d7af6d949b400c4ec1f0128b5
BloopRockets <151418589+BloopRockets@users.noreply.github.com>
2024-10-29 10:14:43 -0400

    added classes, animals, extends

---
+++ wb5demos/src/com/pluralsight/Dog.java
@@ -0,0 +1,17 @@
+package com.pluralsight;
+
+public class Dog extends Animal{
+
+    public String getBreed() {
+        return breed;
+    }
+
+    public void setBreed(String breed) {
+        this.breed = breed;
+    }
+
+    private String breed;
+
+
+
+}
--- wb5demos/src/com/pluralsight/Main.java
+++ wb5demos/src/com/pluralsight/Main.java
@@ -1,2 +1,15 @@
 package com.pluralsight;
- 
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+        Asset a = new Asset();
+        House h = new House();
+
+        h.setValue(100000);
+        a.setValue(15);
+
+
+    }
+}
